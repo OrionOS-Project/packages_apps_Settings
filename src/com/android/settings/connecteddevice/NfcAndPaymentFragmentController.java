dfe41b637adadb8ad6f0a1e6c192ecf79fdd5bcd
NurKeinNeid <nurkeinneid@derpfest.org>
2025-07-25 21:50:44 +0200

    Settings: Improve NFC receiver registration handling
    Signed-off-by: NurKeinNeid <nurkeinneid@derpfest.org>

--- src/com/android/settings/connecteddevice/NfcAndPaymentFragmentController.java
+++ src/com/android/settings/connecteddevice/NfcAndPaymentFragmentController.java
@@ -44,6 +44,7 @@ public class NfcAndPaymentFragmentController extends BasePreferenceController
     private final UserManager mUserManager;
     private final IntentFilter mIntentFilter;
     private Preference mPreference;
+    private boolean mReceiverRegistered = false;
 
     private final BroadcastReceiver mReceiver = new BroadcastReceiver() {
         @Override
@@ -100,20 +101,22 @@ public class NfcAndPaymentFragmentController extends BasePreferenceController
 
     @Override
     public void onStop() {
-        if (!isNfcAvailable()) {
+        if (!isNfcAvailable() || !mReceiverRegistered) {
             return;
         }
 
         mContext.unregisterReceiver(mReceiver);
+        mReceiverRegistered = false;
     }
 
     @Override
     public void onResume() {
-        if (!isNfcAvailable()) {
+        if (!isNfcAvailable() || mReceiverRegistered) {
             return;
         }
 
         mContext.registerReceiver(mReceiver, mIntentFilter);
+        mReceiverRegistered = true;
     }
 
     private boolean isNfcAvailable() {
