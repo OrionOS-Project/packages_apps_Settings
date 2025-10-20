7e2d232c9b88e438770909a08c7e59dc72abdf72
Wrdn28 <ifloraxd@gmail.com>
2025-10-20 19:37:16 +0000

    Settings: Add OrionOS Version to About Phone
    Signed-off-by: Wrdn28 <ifloraxd@gmail.com>

---
+++ src/com/android/settings/deviceinfo/firmwareversion/OrionVersionPreferenceController.java
@@ -0,0 +1,57 @@
+package com.android.settings.deviceinfo.firmwareversion;
+
+import android.content.Context;
+import android.os.SystemProperties;
+import android.text.TextUtils;
+
+import androidx.annotation.VisibleForTesting;
+
+import com.android.settings.R;
+import com.android.settings.core.BasePreferenceController;
+
+public class OrionVersionPreferenceController extends BasePreferenceController {
+
+    @VisibleForTesting
+    static final String ORION_VERSION_PROPERTY = "ro.modversion";
+    private static final String ORION_BASE_VERSION_PROPERTY = "ro.orion.version";
+    private static final String ORION_RELEASE_TYPE_PROPERTY = "ro.orion.build.type";
+
+    public OrionVersionPreferenceController(Context context, String preferenceKey) {
+        super(context, preferenceKey);
+    }
+
+    @Override
+    public int getAvailabilityStatus() {
+        String modVersion = SystemProperties.get(ORION_VERSION_PROPERTY);
+        return !TextUtils.isEmpty(modVersion) ? AVAILABLE : UNSUPPORTED_ON_DEVICE;
+    }
+
+    @Override
+    public CharSequence getSummary() {
+        String baseVersion = SystemProperties.get(ORION_BASE_VERSION_PROPERTY, "");
+        String modVersion = SystemProperties.get(ORION_VERSION_PROPERTY, "");
+        String releaseType = SystemProperties.get(ORION_RELEASE_TYPE_PROPERTY, "");
+
+        if (TextUtils.isEmpty(baseVersion) && TextUtils.isEmpty(modVersion) && TextUtils.isEmpty(releaseType)) {
+            return mContext.getString(R.string.device_info_default);
+        }
+
+        StringBuilder summary = new StringBuilder();
+
+        if (!TextUtils.isEmpty(baseVersion)) {
+            summary.append(baseVersion);
+        }
+
+        if (!TextUtils.isEmpty(modVersion)) {
+            if (summary.length() > 0) summary.append(" | ");
+            summary.append(modVersion);
+        }
+
+        if (!TextUtils.isEmpty(releaseType)) {
+            if (summary.length() > 0) summary.append(" | ");
+            summary.append(releaseType.toUpperCase());
+        }
+
+        return summary.toString();
+    }
+}
